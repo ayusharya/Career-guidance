c297c9d57c86881ca7c9abf1c5d8a204030cb9f8
ayusharya <ayusharya222444@gmail.com>
2018-12-04 18:38:19 -0800

    add the our files

--- Careerduidance/career/career/settings.py
+++ Careerduidance/career/career/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ourpage',
-    'reg',
     'Resister',
     'Resister2',
     'table',
--- Careerduidance/career/reg/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def reg1(request):
-    return render(request,'reg/reg1.html')
