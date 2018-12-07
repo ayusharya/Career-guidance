e8a4e6c8193fa081c78bd057bda2ddfd648be4c5
ayusharya <ayusharya222444@gmail.com>
2018-12-07 22:24:39 +0530

    add all changes

--- Careerduidance/career/career/urls.py
+++ Careerduidance/career/career/urls.py
@@ -6,10 +6,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^ourpage/',include('ourpage.urls')),
-    #url(r'^about/',views.about),
-    #url(r'^$',views.homepage),
-    #url(r'^Register/',views.Register),
-    #url(r'^Register2/',views.Register2),
+    
     url(r'^Resister/',include('Resister.urls')),
     url(r'^Resister2/',include('Resister2.urls')),
     url(r'^table/',include('table.urls')),
--- Careerduidance/career/career/views.py
+++ Careerduidance/career/career/views.py
@@ -1,17 +1,2 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-#def homepage(request):
-    #return HttpResponse('homepage')
-    #return render(request,'homepage.html')
-
-#def about(request):
-    #return HttpResponse('about')
-    #return render(request,'about.html')
-
-#def Register(request):
-    #return HttpResponse('about')
-    #return render(request,'Register.html')
-#def Register2(request):
-    #return HttpResponse('about')
-    #return render(request,'Register2.html')        
